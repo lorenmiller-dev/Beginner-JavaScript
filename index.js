5dca1cca5f72b3b4c087ef131984136cc926f7c9
PreciseJR <jrmiller525@yahoo.com>
2023-07-07 17:56:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Variables
+
+// type of String
+let firstName = "Jr Miller";
+
+// type of number
+let age = 21;
+
+age = age + 1;
+
+// type of boolean
+let student = false;
+
+console.log("Hello " + firstName);
+console.log(age);
+console.log(student);
+
+//
+document.getElementById('p1').innerHTML = "Hello " + firstName
+document.getElementById('p2').innerHTML = "You're " + age + " years old";
+document.getElementById('p3').innerHTML = "Enrolled: " + student
+
+// Aritmetic Expressions
+
