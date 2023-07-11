0316fee51ea9cc10eafac432f4653f1499d9d607
PreciseJR <jrmiller525@yahoo.com>
2023-07-11 01:16:23 -0700

    Some notes on strings and buttons

---
+++ NoteString.js
@@ -0,0 +1,10 @@
+// useful string properties and methods
+
+let username = "jr";
+
+console.log(username.length); // get length of string
+console.log(username.charAt(0)); // get character of string at a given index
+console.log(username.indexOf("j")); // get index of the given character in a string
+console.log(username.lastIndexOf("r")); // get last occuring index of given character from string
+console.log(username.trim); // get rid of empty spaces before and after any characters
+
